e04a5f9fbe44f899ee2c8542d0a1061c82fe5808
Sotadev16 <briankock@hotmail.nl>
2025-09-13 06:19:21 +0200

    Update mail.js

--- api/mail.js
+++ api/mail.js
@@ -9,7 +9,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-
+ 
   try {
     const { name = '', email = '', message = '' } = JSON.parse(req.body || '{}');
 
